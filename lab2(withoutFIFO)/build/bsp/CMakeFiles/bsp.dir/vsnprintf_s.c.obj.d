a43ad1d50645b04c136ae2a5651d410c26dc0692
Martin-YLX <martin_ylx@outlook.com>
2025-03-31 12:58:52 +0800

    Remove build folders from version control

--- lab2(withoutFIFO)/build/bsp/CMakeFiles/bsp.dir/vsnprintf_s.c.obj.d
+++
@@ -1,3 +0,0 @@
-bsp/CMakeFiles/bsp.dir/vsnprintf_s.c.obj: \
- /Users/ylx/Documents/OSLab/lab2(withoutFIFO)/src/bsp/vsnprintf_s.c \
- /Users/ylx/arm-gnu-aarch64-none-elf/lib/gcc/aarch64-none-elf/14.2.1/include/stdarg.h
